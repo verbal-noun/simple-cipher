b17b992790c3c23aaf5ffe0888d7517159e492c8
Kaif Ahsan <kahsan@student.unimelb.edu.au>
2020-06-15 21:13:21 +1000

    Adding option to input text and key from stdin

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -1,19 +1,27 @@
+import java.util.Scanner;
+
 class Encrypt {
     private static final int SHIFT = 25;
     public static void main(String[] args) {
-        String sample = "we found a treasure!";
-        char[] input = sample.toCharArray();
+        Scanner scanner = new Scanner(System.in);
+        String inputText = scanner.nextLine();
+        int key = scanner.nextInt();
+
+        char[] input = inputText.toCharArray();
 
         for(int i = 0; i < input.length; i++) {
-            char letter = sample.charAt(i);
+            char letter = inputText.charAt(i);
             if(letter >= 'a' && letter <= 'z') {
-                letter = (char) ('z' - letter + 'a');
+                letter = (char) (letter + key);
+                if(letter > 'z') {
+                    letter = (char)(letter - 26);
+                }
                 input[i] = letter;
             }
         }
 
         //System.out.println(sample);
-        sample = new String(input);
-        System.out.println(sample);
+        inputText = new String(input);
+        System.out.println(inputText);
     }
 }
